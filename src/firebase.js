34be4c527bff7d4a6903f2451038a968e6a8dce8
Seung taek Oh <elqkddl@naver.com>
2023-02-23 22:29:59 +0900

    01 re-init

---
+++ src/firebase.js
@@ -0,0 +1,14 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBR_U6byxl8Amy38_xyPUNlZgIMTlN1YZA",
+  authDomain: "nwitter-2110f.firebaseapp.com",
+  projectId: "nwitter-2110f",
+  storageBucket: "nwitter-2110f.appspot.com",
+  messagingSenderId: "508591476128",
+  appId: "1:508591476128:web:5db331a7d667b3dc3628c5",
+  measurementId: "G-8M00BK26GD",
+};
+
+export default initializeApp(firebaseConfig);
